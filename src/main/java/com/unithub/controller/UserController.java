b6bdf9bef18037a580a98400fb82047464b70b66
Herbert Gabriel <herbertgacruz@gmail.com>
2025-02-22 18:42:02 -0300

    adiciona AuthService com funcionalidade de login e geração de JWT

--- src/main/java/com/unithub/controller/UserController.java
+++ src/main/java/com/unithub/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.unithub.controller;
 
+import com.unithub.dto.loginDTOs.LoginRequestDTO;
+import com.unithub.dto.loginDTOs.LoginResponseDTO;
 import com.unithub.dto.userDTOs.CreateUserDTO;
+import com.unithub.service.AuthService;
 import com.unithub.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     private final UserService userService;
+    private final AuthService authService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
-
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
+        LoginResponseDTO response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
     @PostMapping("/register")
     public ResponseEntity<Void> newUser(@RequestBody CreateUserDTO dto) {
         userService.createUser(dto);
---
+++ src/main/java/com/unithub/service/AuthService.java
@@ -0,0 +1,58 @@
+package com.unithub.service;
+
+import com.unithub.dto.loginDTOs.LoginRequestDTO;
+import com.unithub.dto.loginDTOs.LoginResponseDTO;
+import com.unithub.model.Role;
+import com.unithub.repository.UserRepository;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+public class AuthService {
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AuthService(JwtEncoder jwtEncoder, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Transactional
+    public LoginResponseDTO login(LoginRequestDTO loginRequest) {
+        var user = userRepository.findByEmail(loginRequest.email());
+
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)) {
+            throw new BadCredentialsException("Invalid email or password");
+        }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+        var roles = user.get().getRoles();
+        var scopes = roles.stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return new LoginResponseDTO(jwtValue, expiresIn);
+    }
+}
