c5570904267027fcc2d09d0b2fcf14f7abd6729e
Herbert Gabriel <herbertgacruz@gmail.com>
2025-03-16 17:51:16 -0300

    remove CheckinController, Checkin model, CheckinService, and EnrolledRepository; adiciona FeedDTO, FeedItemDTO, InscricaoDTO e InscricaoResponseDTO

--- src/main/java/com/unithub/repository/EnrolledRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unithub.repository;
-
-import com.unithub.model.Checkin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnrolledRepository extends JpaRepository<Checkin, Long> {
-}
