05521b77a59a6fb6bee753e4034aa54443a4d65a
Herbert <142347988+herbertgabriel@users.noreply.github.com>
2025-05-15 20:59:07 -0300

    Update EventDetailsDTO.java

--- src/main/java/com/unithub/dto/respose/event/EventDetailsDTO.java
+++ src/main/java/com/unithub/dto/respose/event/EventDetailsDTO.java
@@ -5,14 +5,15 @@ import java.util.Set;
 import java.util.UUID;
 
 public record EventDetailsDTO(
-        UUID eventId,
-        String title,
-        String description,
-        LocalDateTime dateTime,
-        String location,
-        Set<String> categories,
-        boolean active,
-        int maxParticipants,
-        Set<String> images
+    UUID eventId,
+    String title,
+    String description,
+    LocalDateTime dateTime,
+    String location,
+    Set<String> categories,
+    boolean isOfficial,
+    boolean active,
+    int maxParticipants,
+    Set<String> images
 ) {
 }
